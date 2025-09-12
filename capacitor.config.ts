3d4a81a82dcdfd222657591141c64d74ae519768
Elie <eliebouzaglo123@gmail.com>
2025-09-12 17:03:20 -0400

    capacitator.config.ts conflicts fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'net.webnixo.prospector',
-<<<<<<< HEAD
   appName: 'Prospector',
-=======
-  appName: 'prospector',
->>>>>>> origin/main
   webDir: 'dist'
 };
 
